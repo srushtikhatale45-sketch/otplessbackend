4acf985d14e0b8a16f9a93b56d79eb38652ca013
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-06 15:49:11 +0530

    8 th commit

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173','https://otplessfrontend.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:5173','https://otplessfrontend.vercel.app', 'http://otplessfrontend.vercel.app','https://otplessfrontend-r5afptvez-srushtikhatale45-sketchs-projects.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
